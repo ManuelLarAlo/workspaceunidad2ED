56cfd2207ca17f673cb90eed6fb2b7024a16fc61
Manuellaraalos3009 <manuellaraalos@gmail.com>
2023-12-15 11:00:56 +0100

    V 1.3 experimento2

---
+++ prueba1/src/experimento2/AppExperimento2.java
@@ -0,0 +1,12 @@
+package experimento2;
+
+public class AppExperimento2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Tortilla jeje 2, ahora es personal");
+
+	}
+
+}
